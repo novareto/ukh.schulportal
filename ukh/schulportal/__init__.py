1f27160a9405807117e6cf065d978dcb019e6543
Christian Klinger <ck@novareto.de>
2018-03-14 12:03:47 +0100

    chagnes

--- ukh/schulportal/__init__.py
+++ ukh/schulportal/__init__.py
@@ -1,5 +1,14 @@
 import logging
+from SocketServer import BaseServer
+
 logger = logging.getLogger('uvcsite.ukh.schulportal')
+
+
+def my_handle_error(self, request, client_address):
+    return
+
+BaseServer.handle_error = my_handle_error
+
 
 def log(message, summary='', severity=logging.DEBUG):
     logger.log(severity, '%s %s', summary, message)
